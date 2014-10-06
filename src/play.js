c334b9723c7a1732124b5ba646733438e2a5b644
Christopher Hinstorff <chris@chinstorff.com>
2014-10-06 16:41:56 -0700

    paint so that everything isn't redone each time

--- src/play.js
+++ src/play.js
@@ -1,11 +1,14 @@
 Game.Play = function (game) { };
 
-A.num_cells  = 4; // number of cells per row and column
+A.num_cells  = 10; // number of cells per row and column
 A.cell_width = (A.h - 1) / A.num_cells;
 
 A.list = {
     alive: [],
     dead:  [],
+
+    new_alive: [],
+    new_dead:  [],
 };
 
 A.color = {
@@ -15,14 +18,16 @@ A.color = {
 
 A.keys = {};
 
+A.grid_painted = false;
+
 Game.Play.prototype = {
     create: function () {
 	game.stage.backgroundColor = '#ccc';
 	
 	A.grid = new Grid(A.num_cells, A.num_cells);
 
-	A.list.alive.push([Math.floor(Math.random() * A.num_cells), Math.floor(Math.random() * A.num_cells)]);
-
+	this.push_alive([Math.floor(Math.random() * A.num_cells), Math.floor(Math.random() * A.num_cells)]);
+	
 	A.keys.space = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
 	A.keys.space.onDown.add(function () {
 	    this.step();
@@ -33,10 +38,13 @@ Game.Play.prototype = {
     },
 
     update: function () {
-
+	if (A.list.alive.length) {
+	    this.step();
+	}
     },
 
     step: function () {
+	console.log('step');
 	var latest = A.list.alive[A.list.alive.length - 1];
 	var neighbor;
 
@@ -44,10 +52,10 @@ Game.Play.prototype = {
 	    neighbor = A.grid.randomEmptyNeighborOf(latest[0], latest[1]);
 
 	    if (neighbor) {
-		A.list.alive.push(neighbor);
+		this.push_alive(neighbor);
 	    }
 	    else if (A.list.alive.length) {
-		A.list.dead.push(A.list.alive.pop());
+		this.push_dead(this.pop_alive());
 	    }
 	}
 	
@@ -64,17 +72,37 @@ Game.Play.prototype = {
 	}
     },
 
-    paint: function () {
-	if (A.graphics) {
-	    A.graphics.destroy();
+    push_alive: function (val) {
+	A.list.alive.push(val);
+	A.list.new_alive.push(val);
+    },
+
+    push_dead: function (val) {
+	A.list.dead.push(val);
+	A.list.new_dead.push(val);
+    },
+
+    pop_alive: function (val) {
+	var ret = A.list.alive.pop(val);
+	if (A.list.new_alive[A.list.new_alive.length - 1] === ret) {
+	    A.list.new_alive.pop();
 	}
+	return ret;
+    },
+    
+    paint: function () {
+/*	if (A.graphics) {
+	    A.graphics.destroy();
+	}*/
 
 	A.graphics = game.add.graphics(0, 0);
 
-	this.paint_grid();
+	if (!A.grid_painted) {
+	    this.paint_grid();
+	};
 
-	this.paint_list('alive');
-	this.paint_list('dead');
+	this.paint_list('new_alive');
+	this.paint_list('new_dead');
     },
 
     paint_grid: function () {
@@ -85,35 +113,72 @@ Game.Play.prototype = {
 	    this.paint_line(0, dist, A.w, dist);
 	    this.paint_line(dist, 0, dist, A.h);
 	}
+	A.grid_painted = true;
     },
 
-    paint_line: function (x1, y1, x2, y2) {
-	A.graphics.lineStyle(1, 0x000000, 1);
+    paint_line: function (x1, y1, x2, y2, color) {
+	color = color || 0x000000;
+	A.graphics.lineStyle(1, color, 1);
 
 	A.graphics.moveTo(x1, y1);
 	A.graphics.lineTo(x2, y2);
     },
 
     paint_list: function (name) {
-	var cell;
+	var cell, index, prior, x, y;
 	for (var i = 0; i < A.list[name].length; i++) {
 	    cell = A.list[name][i];
-	    this.paint_cell(cell[0], cell[1], A.color[name]);
+
+	    if (name === 'new_alive') {
+		this.paint_cell_alive(cell[0], cell[1]);
+	    }
+	    else if (name === 'new_dead') {
+		this.paint_cell_dead(cell[0], cell[1]);
+	    }
+
+	    index = this.indexOf_2d_array(A.list.alive, cell);
+	    if (index && name === 'new_alive') {
+		prior = A.list.alive[index-1];
+		val_x = Math.floor(cell[0] * A.cell_width);
+		val_y = Math.floor(cell[1] * A.cell_width);
+		
+		if (cell[0] - prior[0] === 1) {
+		    this.paint_line(val_x, val_y + 1, val_x, val_y + A.cell_width - 1, A.color.dead);
+		}
+		if (cell[0] - prior[0] === -1) {
+		    val_x += A.cell_width;
+		    this.paint_line(val_x, val_y + 1, val_x, val_y + A.cell_width - 1, A.color.dead);
+		}
+		if (cell[1] - prior[1] === 1) {
+		    this.paint_line(val_x + 1, val_y, val_x + A.cell_width - 1, val_y, A.color.dead);
+		}
+		if (cell[1] - prior[1] === -1) {
+		    val_y += A.cell_width;
+		    this.paint_line(val_x + 1, val_y, val_x + A.cell_width - 1, val_y, A.color.dead);
+		}
+	    }
 	}
+	A.list[name] = [];
     },
 
-    paint_cell: function (x, y, color, opening) {
+    paint_cell_alive: function (x, y) {
+	var offset = Math.floor(A.cell_width / 5 * 1);
+
+	this.paint_cell_dead(x, y);
+	this.paint_cell(x, y, A.color.alive, offset);
+    },
+    
+    paint_cell_dead: function (x, y) {
+	this.paint_cell(x, y, A.color.dead);
+    },
+    
+    paint_cell: function (x, y, color, offset) {
+	offset = offset || 0;
+
 	val_x = Math.floor(x * A.cell_width);
 	val_y = Math.floor(y * A.cell_width);
 
-	var offset = {
-	    x: 0,
-	    y: 0,
-	    wx: 0,
-	    
-	};
-	
-	this.paint_rect(val_x, val_y, A.cell_width, color);
+	this.paint_rect(val_x + offset, val_y + offset, A.cell_width - 2 * offset, color);
     },
     
     paint_rect: function (x, y, width, color) {
@@ -121,4 +186,21 @@ Game.Play.prototype = {
 	A.graphics.beginFill(color, 1);
 	A.graphics.drawRect(x+1, y+1, width-1, width-1);
     },
+
+    indexOf_2d_array: function (arr_search, arr_value) {
+	var matching, i, j;
+	for (i = 0; i < arr_search.length; i++) {
+	    matching = true;
+
+	    for (j = 0; j < arr_value.length; j++) {
+		if (arr_search[i][j] !== arr_value[j]) {
+		    matching = false;
+		}   
+	    }
+
+	    if (matching) {
+		return i;
+	    }
+	}
+    }
 };
